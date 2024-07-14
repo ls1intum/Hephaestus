e4c4156a26d2303d94c66e9251af3b2cd33ba485
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-07-14 18:09:18 +0200

    change to zoneless change detection

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloWorldComponent],
   templateUrl: './app.component.html',
   styles: [],
 })
--- webapp/src/app/hello-world/hello-world.component.ts
+++ webapp/src/app/hello-world/hello-world.component.ts
@@ -1,12 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'hello-world',
   standalone: true,
-  imports: [],
   templateUrl: './hello-world.component.html',
-  styles: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HelloWorldComponent {
 
