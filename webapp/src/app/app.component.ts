1d16d76337348d132846d63f5e37856e6df3d252
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-07-15 15:13:59 +0200

    add a example counter using signals

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { CounterComponent } from './example/counter/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HelloWorldComponent],
+  imports: [RouterOutlet, CounterComponent],
   templateUrl: './app.component.html',
   styles: [],
 })
