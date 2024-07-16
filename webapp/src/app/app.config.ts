0cbcb290395006aee7a1c96f5b0cb010120646a5
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-07-16 19:28:55 +0200

    add base path

--- webapp/src/app/app.config.ts
+++ webapp/src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { BASE_PATH } from './core/modules/openapi';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: BASE_PATH, useValue: "https://pokeapi.co" },
     provideExperimentalZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
   ]
 };
