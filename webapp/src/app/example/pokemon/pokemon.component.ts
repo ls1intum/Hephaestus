72f06d47325223a11d4de56902eb069a80c5089a
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-07-16 19:32:19 +0200

    add pokemon component and service

---
+++ webapp/src/app/example/pokemon/pokemon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pokemon',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokemon.component.html'
+})
+export class PokemonComponent {
+
+}
