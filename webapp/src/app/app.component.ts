1b2b72b842bfd385dc39e935283caae75eee2d60
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-07-17 13:55:40 +0200

    add pokemon example

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from './example/counter/counter.component';
+import { PokemonComponent } from './example/pokemon/pokemon.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CounterComponent],
+  imports: [RouterOutlet, CounterComponent, PokemonComponent],
   templateUrl: './app.component.html',
   styles: [],
 })
--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts}'],
+  content: [__dirname + '/src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
