1a977f41f08c0d785d683c4a10afe2229684177a
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-08-01 09:54:24 +0200

    fix naming

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from './example/counter/counter.component';
-import { PokemonComponent } from './example/hello/hello.component';
+import { HelloComponent } from './example/hello/hello.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CounterComponent, PokemonComponent],
+  imports: [RouterOutlet, CounterComponent, HelloComponent],
   templateUrl: './app.component.html',
   styles: [],
 })
