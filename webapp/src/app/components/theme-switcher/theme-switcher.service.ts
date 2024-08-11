a48602d34b0b272d3a9f42c478d7c03f0cef715a
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2024-08-11 20:10:31 +0200

    Add theme switcher to header (#45)

---
+++ webapp/src/app/components/theme-switcher/theme-switcher.service.ts
@@ -0,0 +1,71 @@
+import { Injectable, signal } from '@angular/core';
+
+export enum AppTheme {
+  LIGHT = 'light',
+  DARK = 'dark'
+}
+
+const IS_CLIENT_RENDER = typeof localStorage !== 'undefined';
+const LOCAL_STORAGE_THEME_KEY = 'theme';
+
+let selectedTheme: AppTheme | undefined = undefined;
+
+if (IS_CLIENT_RENDER) {
+  selectedTheme = (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as AppTheme) || undefined;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeSwitcherService {
+  currentTheme = signal<AppTheme | undefined>(selectedTheme);
+
+  setLightTheme() {
+    this.currentTheme.set(AppTheme.LIGHT);
+    this.setToLocalStorage(AppTheme.LIGHT);
+    this.removeClassFromHtml('dark');
+  }
+
+  setDarkTheme() {
+    this.currentTheme.set(AppTheme.DARK);
+    this.setToLocalStorage(AppTheme.DARK);
+    this.addClassToHtml('dark');
+  }
+
+  setSystemTheme() {
+    this.currentTheme.set(undefined);
+    this.removeFromLocalStorage();
+
+    const isSystemDark = window?.matchMedia('(prefers-color-scheme: dark)').matches ?? false;
+    if (isSystemDark) {
+      this.addClassToHtml('dark');
+    } else {
+      this.removeClassFromHtml('dark');
+    }
+  }
+
+  private addClassToHtml(className: string) {
+    if (IS_CLIENT_RENDER) {
+      this.removeClassFromHtml(className);
+      document.documentElement.classList.add(className);
+    }
+  }
+
+  private removeClassFromHtml(className: string) {
+    if (IS_CLIENT_RENDER) {
+      document.documentElement.classList.remove(className);
+    }
+  }
+
+  private setToLocalStorage(theme: AppTheme) {
+    if (IS_CLIENT_RENDER) {
+      localStorage.setItem(LOCAL_STORAGE_THEME_KEY, theme);
+    }
+  }
+
+  private removeFromLocalStorage() {
+    if (IS_CLIENT_RENDER) {
+      localStorage.removeItem(LOCAL_STORAGE_THEME_KEY);
+    }
+  }
+}
