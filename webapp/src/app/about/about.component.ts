a88526fd0e86f8a2e1ea3dc45c884d470ccd4f40
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-08-12 19:54:33 +0200

    Create About Page in Footer Fixes #22

---
+++ webapp/src/app/about/about.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './about.component.html'
+})
+export class AboutComponent {}
