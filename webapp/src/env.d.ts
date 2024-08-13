e8223e21c9c688864456865af2c2a647a21eb572
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-08-14 00:17:33 +0200

    revert again

--- webapp/src/env.d.ts
+++
@@ -1,8 +0,0 @@
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
-interface ImportMetaEnv {
-  readonly NODE_ENV: string;
-  readonly NG_APPLICATION_SERVER_URL: string;
-}
--- webapp/src/environments/environment.production.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-  serverUrl: import.meta.env.NG_APPLICATION_SERVER_URL
-};
