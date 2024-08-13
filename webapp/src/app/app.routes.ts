8283e3bd2fc1f610b175d44c9a66d68b3dc4f283
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2024-08-13 15:38:54 +0200

    Create about page and add footer (#52)

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from 'app/about/about.component';
+import { MainComponent } from 'app/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'about', component: AboutComponent }
+];
