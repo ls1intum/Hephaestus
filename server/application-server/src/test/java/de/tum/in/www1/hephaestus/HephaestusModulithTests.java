d46377ed317a32c27a311b876cb322a21cdde782
Armin Stanitzok <21990230+GODrums@users.noreply.github.com>
2024-08-16 09:33:30 +0200

    Integrate Spring Modulith in `application-server` (#59)
    Co-authored-by: Felix T.J. Dietrich <felixtj.dietrich@tum.de>

---
+++ server/application-server/src/test/java/de/tum/in/www1/hephaestus/HephaestusModulithTests.java
@@ -0,0 +1,28 @@
+package de.tum.in.www1.hephaestus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
+import org.springframework.modulith.docs.Documenter.DiagramOptions;
+import org.springframework.modulith.docs.Documenter.DiagramOptions.DiagramStyle;
+
+public class HephaestusModulithTests {
+
+    ApplicationModules modules = ApplicationModules.of(Application.class);
+
+    @Test
+    void shouldBeCompliant() {
+        modules.forEach(System.out::println);
+        modules.verify();
+    }
+
+    @Test
+    void writeDocumentationSnippets() {
+        DiagramOptions options = DiagramOptions.defaults().withStyle(DiagramStyle.UML);
+        new Documenter(modules)
+                .writeModuleCanvases()
+                .writeModulesAsPlantUml(options)
+                .writeIndividualModulesAsPlantUml(options);
+    }
+
+}
