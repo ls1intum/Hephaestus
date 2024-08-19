88bc77d9076c4a849deaa8ccdbde295a55ca29dd
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-08-19 21:56:37 +0200

    add healthcheck

--- server/webhook-ingest/app/main.py
+++ server/webhook-ingest/app/main.py
@@ -1,7 +1,8 @@
 import hmac
 import hashlib
 from contextlib import asynccontextmanager
-from fastapi import Body, FastAPI, HTTPException, Header, Request
+from fastapi import Body, FastAPI, HTTPException, Header, Request, status
+from pydantic import BaseModel
 from nats.js.api import StreamConfig
 from app.config import settings
 from app.nats_client import nats_client
@@ -58,3 +59,30 @@ async def github_webhook(
     await nats_client.js.publish(subject, body)
 
     return { "status": "ok" }
+
+
+class HealthCheck(BaseModel):
+    """Response model to validate and return when performing a health check."""
+
+    status: str = "OK"
+
+
+@app.get(
+    "/health",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
+    to ensure a robust container orchestration and management is in place. Other
+    services which rely on proper functioning of the API service will not deploy if this
+    endpoint returns any other HTTP status code except 200 (OK).
+    Returns:
+        HealthCheck: Returns a JSON response with the health status
+    """
+    return HealthCheck(status="OK")
