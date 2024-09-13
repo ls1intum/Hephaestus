6369b5d377e5bf4b70e89bdb18c93a3188c6d144
GODrums <armin.stanitzok@outlook.de>
2024-09-13 20:03:51 +0200

    fix: delete leftovers from merge

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/codereview/comment/review/ReviewCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package de.tum.in.www1.hephaestus.codereview.comment.review;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewCommentRepository extends JpaRepository<ReviewComment, Long> {
-
-}
