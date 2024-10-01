987e08e0f67bb74c31d2d4f1022131048e45e04e
milenasrb <mi.serbinova@gmail.com>
2024-10-01 17:07:02 +0200

    Format

--- server/intelligence-service/app/main.py
+++ server/intelligence-service/app/main.py
@@ -4,15 +4,19 @@ from .model import model
 
 app = FastAPI()
 
+
 class ChatRequest(BaseModel):
     message: str
+
+
 class ChatResponse(BaseModel):
     response: str
+
 
 @app.post("/chat", response_model=ChatResponse, summary="Chat with LLM")
 async def chat(request: ChatRequest):
     try:
         response = model.invoke(request.message)
-        return { "response": response.content }
+        return {"response": response.content}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
