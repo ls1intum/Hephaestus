64af0391bd0fb3bc24c2398d4933f28b8d453420
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-10-19 01:09:09 +0200

    fix ring offset color

--- webapp/src/libs/ui/ui-card-helm/src/lib/hlm-card.directive.ts
+++ webapp/src/libs/ui/ui-card-helm/src/lib/hlm-card.directive.ts
@@ -3,15 +3,18 @@ import { hlm } from '@spartan-ng/ui-core';
 import { type VariantProps, cva } from 'class-variance-authority';
 import type { ClassValue } from 'clsx';
 
-export const cardVariants = cva('rounded-lg border border-border bg-card focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-card-foreground shadow-sm', {
-  variants: {
-    variant: {
-      default: '',
-      profile: 'shadow-md block p-4'
-    }
-  },
-  defaultVariants: {}
-});
+export const cardVariants = cva(
+  'rounded-lg border border-border bg-card focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-background focus:ring-offset-2 text-card-foreground shadow-sm',
+  {
+    variants: {
+      variant: {
+        default: '',
+        profile: 'shadow-md block p-4'
+      }
+    },
+    defaultVariants: {}
+  }
+);
 export type CardVariants = VariantProps<typeof cardVariants>;
 
 @Directive({
