237578f8a718c9ef68ea2203a50d719c0b09effd
GODrums <armin.stanitzok@outlook.de>
2024-10-21 01:19:30 +0200

    Get meta data from config

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/meta/MetaService.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/meta/MetaService.java
@@ -2,18 +2,21 @@ package de.tum.in.www1.hephaestus.meta;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import de.tum.in.www1.hephaestus.admin.AdminService;
 
 @Service
 public class MetaService {
     private static final Logger logger = LoggerFactory.getLogger(MetaService.class);
 
-    @Value("${monitoring.repositories}")
-    private String[] repositoriesToMonitor;
+    @Autowired
+    private AdminService adminService;
 
     public MetaDataDTO getMetaData() {
         logger.info("Getting meta data...");
-        return new MetaDataDTO(repositoriesToMonitor);
+        return new MetaDataDTO(
+                adminService.getAdminConfig().getRepositoriesToMonitor().stream().sorted().toArray(String[]::new));
     }
 }
