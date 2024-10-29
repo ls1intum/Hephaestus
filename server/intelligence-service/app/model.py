0a3c1c404b5f1ff6d1317cceb5c207ea8d8c1826
milenasrb <mi.serbinova@gmail.com>
2024-10-29 19:41:51 +0100

    Add new GitHub workflow

--- server/intelligence-service/app/model.py
+++ server/intelligence-service/app/model.py
@@ -1,9 +1,16 @@
+import os
 from langchain.chat_models.base import BaseChatModel
 from langchain_openai import ChatOpenAI, AzureChatOpenAI
 
 from .config import settings
 
 model: BaseChatModel
+
+if os.getenv("GITHUB_ACTIONS") == "true":
+    # mock model for testing environment
+    class MockChatModel(BaseChatModel):
+        def invoke(self, message: str):
+            return "Mock response"
 
 if settings.is_openai_available:
     model = ChatOpenAI()
