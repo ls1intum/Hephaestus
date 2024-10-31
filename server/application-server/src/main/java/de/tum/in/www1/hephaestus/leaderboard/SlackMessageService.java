fb5aa50e3737f8dfc951190856ae0b44bae91043
GODrums <armin.stanitzok@outlook.de>
2024-10-31 15:41:05 +0100

    Add env vars

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/leaderboard/SlackMessageService.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/leaderboard/SlackMessageService.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -25,18 +27,27 @@ import static com.slack.api.model.block.composition.BlockCompositions.*;
 import static com.slack.api.model.block.element.BlockElements.*;
 
 @Order(value = Ordered.LOWEST_PRECEDENCE)
+@EnableScheduling
 @Service
 public class SlackMessageService {
 
     private static final Logger logger = LoggerFactory.getLogger(SlackMessageService.class);
 
+    @Value("${slack.channelId}")
+    private String channelId;
+
+    @Value("${slack.runScheduledMessage}")
+    private boolean runScheduledMessage;
+
     @Autowired
     private App slackApp;
 
-    public void sendMessage(String channelId, List<LayoutBlock> blocks) throws IOException, SlackApiException {
+    public void sendMessage(String channelId, List<LayoutBlock> blocks, String fallback)
+            throws IOException, SlackApiException {
         ChatPostMessageRequest request = ChatPostMessageRequest.builder()
                 .channel(channelId)
                 .blocks(blocks)
+                .text(fallback) // used for example in notifications
                 .build();
 
         ChatPostMessageResponse response = slackApp.client().chatPostMessage(request);
@@ -46,20 +57,24 @@ public class SlackMessageService {
         }
     }
 
-    @Scheduled(cron = "0 0 9 ? * TUE")
+    @Scheduled(cron = "${slack.cronSchedule}")
     public void sendScheduledLeaderboard() {
+        if (!runScheduledMessage) {
+            return;
+        }
+        logger.info("Sending scheduled message to Slack channel...");
+        var blocks = asBlocks(
+                header(header -> header.text(
+                        plainText(pt -> pt.text(":newspaper:  Developer Reviews of the last week  :newspaper:")))),
+                divider(),
+                context(context -> context
+                        .elements(List.of(markdownText("*October 31, 2024*  |  Hephaestus Announcement")))),
+                section(section -> section.text(markdownText(
+                        "Here is the *review table* summary for the last week. The table is taken directly from <https://hephaestus.ase.cit.tum.de/|Hephaestus> and now also includes non-review comments & code comments in addition to all variants of reviews for each developer. The score is a weighted heuristic based on the complexities of the reviewed PRs (similar to the old algorithm)."))),
+                section(section -> section.text(markdownText(
+                        "If you would like to check out more leaderboards or statistics, head over to last week's leaderboard or this month's leaderboard."))));
         try {
-            var blocks = asBlocks(
-                    header(header -> header.text(
-                            plainText(pt -> pt.text(":newspaper:  Developer Reviews of the last week  :newspaper:")))),
-                    divider(),
-                    context(context -> context
-                            .elements(List.of(markdownText("*October 31, 2024*  |  Hephaestus Announcement")))),
-                    section(section -> section.text(markdownText(
-                            "Here is the *review table* summary for the last week. The table is taken directly from <https://hephaestus.ase.cit.tum.de/|Hephaestus> and now also includes non-review comments & code comments in addition to all variants of reviews for each developer. The score is a weighted heuristic based on the complexities of the reviewed PRs (similar to the old algorithm)."))),
-                    section(section -> section.text(markdownText(
-                            "If you would like to check out more leaderboards or statistics, head over to last week's leaderboard or this month's leaderboard."))));
-            sendMessage("C07CZNLULQN", blocks);
+            sendMessage(channelId, blocks, "Developer Reviews of the last week");
         } catch (IOException | SlackApiException e) {
             logger.error("Failed to send scheduled message to Slack channel: " + e.getMessage());
         }
@@ -70,6 +85,10 @@ public class SlackMessageService {
      */
     @EventListener(ApplicationReadyEvent.class)
     public void run() {
+        if (!runScheduledMessage) {
+            logger.info("Slack scheduled messages are disabled, skipping Slack app init test.");
+            return;
+        }
         AuthTestResponse response;
         try {
             response = slackApp.client().authTest(r -> r);
