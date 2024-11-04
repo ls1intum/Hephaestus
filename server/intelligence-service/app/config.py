32dfb6d6691e6f7d252f5f3edcb14c78767352aa
Milena Serbinova <45200178+milesha@users.noreply.github.com>
2024-11-04 22:05:03 +0100

    Generate Java API Client for Intelligence Service's OpenAPI Specifications  (#106)
    Co-authored-by: Felix T.J. Dietrich <felix_dietrich@gmx.de>
    Co-authored-by: Felix T.J. Dietrich <felixtj.dietrich@tum.de>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- server/intelligence-service/app/config.py
+++ server/intelligence-service/app/config.py
@@ -1,7 +1,12 @@
-from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
     OPENAI_API_KEY: str = ""
 
     AZURE_OPENAI_API_KEY: str = ""
@@ -11,9 +16,11 @@ class Settings(BaseSettings):
     @property
     def is_openai_available(self):
         return bool(self.OPENAI_API_KEY)
-    
+
     @property
     def is_azure_openai_available(self):
-        return bool(self.AZURE_OPENAI_API_KEY) and bool(self.AZURE_OPENAI_ENDPOINT) and bool(self.AZURE_OPENAI_API_VERSION)
+        return bool(self.AZURE_OPENAI_API_KEY) and bool(self.AZURE_OPENAI_ENDPOINT) and bool(
+            self.AZURE_OPENAI_API_VERSION)
+
 
 settings = Settings()
