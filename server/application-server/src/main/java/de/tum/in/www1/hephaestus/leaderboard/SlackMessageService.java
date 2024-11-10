170155e469e7e4df03d68b6d372a09dd0219e24d
GODrums <armin.stanitzok@outlook.de>
2024-11-10 22:39:40 +0100

    Make Slack scheduler dynamic

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/leaderboard/SlackMessageService.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/leaderboard/SlackMessageService.java
@@ -1,5 +1,15 @@
 package de.tum.in.www1.hephaestus.leaderboard;
 
+import static com.slack.api.model.block.Blocks.*;
+import static com.slack.api.model.block.composition.BlockCompositions.*;
+
+import com.slack.api.bolt.App;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import com.slack.api.methods.response.auth.AuthTestResponse;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
+import com.slack.api.model.User;
+import com.slack.api.model.block.LayoutBlock;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
@@ -7,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
-
+import org.apache.commons.text.similarity.LevenshteinDistance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,20 +27,10 @@ import org.springframework.context.event.EventListener;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.CronExpression;
+import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Service;
-
-import com.slack.api.bolt.App;
-import com.slack.api.methods.SlackApiException;
-import com.slack.api.methods.request.chat.ChatPostMessageRequest;
-import com.slack.api.methods.response.auth.AuthTestResponse;
-import com.slack.api.methods.response.chat.ChatPostMessageResponse;
-import com.slack.api.model.User;
-import com.slack.api.model.block.LayoutBlock;
-import org.apache.commons.text.similarity.LevenshteinDistance;
-
-import static com.slack.api.model.block.Blocks.*;
-import static com.slack.api.model.block.composition.BlockCompositions.*;
 
 @Order(value = Ordered.LOWEST_PRECEDENCE)
 @EnableScheduling
@@ -39,20 +39,29 @@ public class SlackMessageService {
 
     private static final Logger logger = LoggerFactory.getLogger(SlackMessageService.class);
 
-    @Value("${hephaestus.notification.leaderboard.channelId}")
+    @Value("${hephaestus.leaderboard.notification.channelId}")
     private String channelId;
 
-    @Value("${hephaestus.notification.leaderboard.enabled}")
+    @Value("${hephaestus.leaderboard.notification.enabled}")
     private boolean runScheduledMessage;
 
     @Value("${hephaestus.host_url:localhost:8080}")
     private String hephaestusUrl;
+
+    @Value("${hephaestus.leaderboard.schedule.day}")
+    private String scheduledDay;
+
+    @Value("${hephaestus.leaderboard.schedule.time}")
+    private String scheduledTime;
 
     @Autowired
     private App slackApp;
 
     @Autowired
     private LeaderboardService leaderboardService;
+
+    @Autowired
+    private ThreadPoolTaskScheduler taskScheduler;
 
     /**
      * Sends a message to the specified Slack channel.
@@ -63,12 +72,12 @@ public class SlackMessageService {
      * @throws SlackApiException
      */
     public void sendMessage(String channelId, List<LayoutBlock> blocks, String fallback)
-            throws IOException, SlackApiException {
+        throws IOException, SlackApiException {
         ChatPostMessageRequest request = ChatPostMessageRequest.builder()
-                .channel(channelId)
-                .blocks(blocks)
-                .text(fallback) // used for example in notifications
-                .build();
+            .channel(channelId)
+            .blocks(blocks)
+            .text(fallback) // used for example in notifications
+            .build();
 
         ChatPostMessageResponse response = slackApp.client().chatPostMessage(request);
 
@@ -77,76 +86,23 @@ public class SlackMessageService {
         }
     }
 
-    /**
-     * Gets the Slack handles of the top 3 reviewers of the last week.
-     * @return
-     */
-    private List<User> getTop3SlackReviewers() {
-        // exactly 7 days ago
-        OffsetDateTime after = OffsetDateTime.of(LocalDate.now().minusDays(7), OffsetDateTime.now().toLocalTime(),
-                OffsetDateTime.now().getOffset());
-        var leaderboard = leaderboardService.createLeaderboard(after, OffsetDateTime.now(), Optional.empty());
-        var top3 = leaderboard.subList(0, Math.min(3, leaderboard.size()));
-        logger.debug("Top 3 Users of the last week: " + top3.stream().map(e -> e.user().name()).toList());
-
-        List<User> allSlackUsers;
-        try {
-            allSlackUsers = slackApp.client().usersList(r -> r).getMembers();
-        } catch (SlackApiException | IOException e) {
-            logger.error("Failed to get Slack users: " + e.getMessage());
-            return new ArrayList<>();
-        }
-
-        return top3.stream().map(entry -> {
-            var exactUser = allSlackUsers.stream()
-                .filter(user -> user.getName().equals(entry.user().name()) || (user.getProfile().getEmail() != null && user.getProfile().getEmail().equals(entry.user().email()))).findFirst();
-            if (exactUser.isPresent()) {
-                return exactUser.get();
-            }
-
-            // find through String edit distance
-            return allSlackUsers.stream()
-                .min((a, b) -> Integer.compare(
-                    LevenshteinDistance.getDefaultInstance().apply(
-                        entry.user().name(), a.getName()),
-                    LevenshteinDistance.getDefaultInstance().apply(
-                        entry.user().name(), b.getName()))).orElse(null);
-        }).filter(user -> user != null).toList();
-    }
-
-    @Scheduled(cron = "${hephaestus.notification.leaderboard.cron}")
-    public void sendScheduledLeaderboard() {
+    @EventListener(ApplicationReadyEvent.class)
+    public void activateTaskScheduler() {
         if (!runScheduledMessage) {
             return;
         }
 
-        // get date in unix format
-        var currentDate = OffsetDateTime.now().toEpochSecond();
+        var timeParts = scheduledTime.split(":");
 
-        var top3reviewers = getTop3SlackReviewers();
-
-        logger.info("Sending scheduled message to Slack channel...");
-        List<LayoutBlock> blocks = asBlocks(
-                header(header -> header.text(
-                        plainText(pt -> pt.text(":newspaper: Reviews of the last week :newspaper:")))),
-                context(context -> context
-                        .elements(List.of(markdownText("<!date^" + currentDate + "^{date} at {time}| Today at 9:00AM CEST> | " + hephaestusUrl)))),
-                divider(),
-                section(section -> section.text(markdownText(
-                        "Another *review leaderboard* has concluded. You can check out your placement <" + hephaestusUrl + "|here>."))),
-                section(section -> section.text(markdownText(
-                        "Special thanks to our top 3 reviewers of last week:"))),
-                section(section -> section.text(markdownText(
-                        IntStream.range(0, top3reviewers.size())
-                                .mapToObj(i -> ((i + 1) + ". <@" + top3reviewers.get(i).getId() + ">"))
-                                .reduce((a, b) -> a + "\n" + b).orElse("")))),
-                section(section -> section.text(markdownText("Happy coding and reviewing! :rocket:")))
-        );
-        try {
-            sendMessage(channelId, blocks, "Reviews of the last week");
-        } catch (IOException | SlackApiException e) {
-            logger.error("Failed to send scheduled message to Slack channel: " + e.getMessage());
+        String cron = String.format("0 %s %s ? * %s", timeParts.length > 1 ? timeParts[1] : 0, timeParts[0], scheduledDay);
+        
+        if (!CronExpression.isValidExpression(cron)) {
+            logger.error("Invalid cron expression: " + cron);
+            return;
         }
+
+        logger.info("Scheduling Slack message to run with {}", cron);
+        taskScheduler.schedule(new SlackWeeklyLeaderboardTask(), new CronTrigger(cron));
     }
 
     /**
@@ -171,6 +127,118 @@ public class SlackMessageService {
             logger.info("Slack app is successfully initialized.");
         } else {
             logger.error("Failed to initialize Slack app: " + response.getError());
+        }
+    }
+
+    /**
+     * Task to send a weekly leaderboard message to the Slack channel.
+     * @see SlackMessageService#activateTaskScheduler()
+     */
+    private class SlackWeeklyLeaderboardTask implements Runnable {
+
+        /**
+         * Gets the Slack handles of the top 3 reviewers of the last week.
+         * @return
+         */
+        private List<User> getTop3SlackReviewers() {
+            // exactly 7 days ago
+            OffsetDateTime after = OffsetDateTime.of(
+                LocalDate.now().minusDays(7),
+                OffsetDateTime.now().toLocalTime(),
+                OffsetDateTime.now().getOffset()
+            );
+            var leaderboard = leaderboardService.createLeaderboard(after, OffsetDateTime.now(), Optional.empty());
+            var top3 = leaderboard.subList(0, Math.min(3, leaderboard.size()));
+            logger.debug("Top 3 Users of the last week: " + top3.stream().map(e -> e.user().name()).toList());
+
+            List<User> allSlackUsers;
+            try {
+                allSlackUsers = slackApp.client().usersList(r -> r).getMembers();
+            } catch (SlackApiException | IOException e) {
+                logger.error("Failed to get Slack users: " + e.getMessage());
+                return new ArrayList<>();
+            }
+
+            return top3
+                .stream()
+                .map(entry -> {
+                    var exactUser = allSlackUsers
+                        .stream()
+                        .filter(
+                            user ->
+                                user.getName().equals(entry.user().name()) ||
+                                (user.getProfile().getEmail() != null &&
+                                    user.getProfile().getEmail().equals(entry.user().email()))
+                        )
+                        .findFirst();
+                    if (exactUser.isPresent()) {
+                        return exactUser.get();
+                    }
+
+                    // find through String edit distance
+                    return allSlackUsers
+                        .stream()
+                        .min((a, b) ->
+                            Integer.compare(
+                                LevenshteinDistance.getDefaultInstance().apply(entry.user().name(), a.getName()),
+                                LevenshteinDistance.getDefaultInstance().apply(entry.user().name(), b.getName())
+                            )
+                        )
+                        .orElse(null);
+                })
+                .filter(user -> user != null)
+                .toList();
+        }
+
+        @Override
+        public void run() {
+            // get date in unix format
+            var currentDate = OffsetDateTime.now().toEpochSecond();
+
+            var top3reviewers = getTop3SlackReviewers();
+
+            logger.info("Sending scheduled message to Slack channel...");
+            List<LayoutBlock> blocks = asBlocks(
+                header(header ->
+                    header.text(plainText(pt -> pt.text(":newspaper: Reviews of the last week :newspaper:")))
+                ),
+                context(context ->
+                    context.elements(
+                        List.of(
+                            markdownText(
+                                "<!date^" + currentDate + "^{date} at {time}| Today at 9:00AM CEST> | " + hephaestusUrl
+                            )
+                        )
+                    )
+                ),
+                divider(),
+                section(section ->
+                    section.text(
+                        markdownText(
+                            "Another *review leaderboard* has concluded. You can check out your placement <" +
+                            hephaestusUrl +
+                            "|here>."
+                        )
+                    )
+                ),
+                section(section -> section.text(markdownText("Special thanks to our top 3 reviewers of last week:"))),
+                section(section ->
+                    section.text(
+                        markdownText(
+                            IntStream.range(0, top3reviewers.size())
+                                .mapToObj(i -> ((i + 1) + ". <@" + top3reviewers.get(i).getId() + ">"))
+                                .reduce((a, b) -> a + "\n" + b)
+                                .orElse("")
+                        )
+                    )
+                ),
+                section(section -> section.text(markdownText("Happy coding and reviewing! :rocket:")))
+            );
+            try {
+                sendMessage(channelId, blocks, "Reviews of the last week");
+            } catch (IOException | SlackApiException e) {
+                logger.error("Failed to send scheduled message to Slack channel: " + e.getMessage());
+            }
         }
     }
 }
