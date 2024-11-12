57cb8e4912b015873226a57eda1e75723222ad95
GODrums <armin.stanitzok@outlook.de>
2024-11-12 14:14:25 +0100

    Testing Timezone change

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/Application.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/Application.java
@@ -1,5 +1,6 @@
 package de.tum.in.www1.hephaestus;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+        System.out.println("Old TimeZone: " + TimeZone.getDefault().toString());
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Berlin"));
+		System.out.println("New TimeZone: " + TimeZone.getDefault().toString());
 		SpringApplication.run(Application.class, args);
 	}
 }
