bc1125c1ff22504bde5a834e0337dc69195f923a
GODrums <armin.stanitzok@outlook.de>
2024-11-12 14:54:42 +0100

    Remove logging

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/Application.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/Application.java
@@ -8,9 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-        System.out.println("Old TimeZone: " + TimeZone.getDefault().toString());
 		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Berlin"));
-		System.out.println("New TimeZone: " + TimeZone.getDefault().toString());
 		SpringApplication.run(Application.class, args);
 	}
 }
