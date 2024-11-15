6312a46503e6ede7d84480fc0e6bbe129b6415a4
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-11-15 18:04:18 +0100

    fix minor issues, componetize, add storybook

--- webapp/src/app/home/leaderboard/leaderboard.component.ts
+++ webapp/src/app/home/leaderboard/leaderboard.component.ts
@@ -1,25 +1,19 @@
-import { Component, inject, input, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Component, inject, input } from '@angular/core';
 import { NgIconComponent } from '@ng-icons/core';
 import { octFileDiff, octCheck, octComment, octCommentDiscussion, octGitPullRequest, octChevronLeft, octNoEntry } from '@ng-icons/octicons';
-import { LeaderboardEntry, type PullRequestInfo } from 'app/core/modules/openapi';
+import { cn } from '@app/utils';
+import { HlmAvatarModule } from '@spartan-ng/ui-avatar-helm';
+import { HlmSkeletonModule } from '@spartan-ng/ui-skeleton-helm';
+import { SecurityStore } from '@app/core/security/security-store.service';
+import { LeaderboardEntry } from 'app/core/modules/openapi';
 import { TableBodyDirective } from 'app/ui/table/table-body.directive';
 import { TableCellDirective } from 'app/ui/table/table-cell.directive';
 import { TableHeadDirective } from 'app/ui/table/table-head.directive';
 import { TableHeaderDirective } from 'app/ui/table/table-header.directive';
 import { TableRowDirective } from 'app/ui/table/table-row.directive';
 import { TableComponent } from 'app/ui/table/table.component';
-import { HlmAvatarModule } from '@spartan-ng/ui-avatar-helm';
-import { HlmSkeletonModule } from '@spartan-ng/ui-skeleton-helm';
-import { HlmPopoverModule } from '@spartan-ng/ui-popover-helm';
-import { HlmButtonModule } from '@spartan-ng/ui-button-helm';
-import { BrnPopoverComponent, BrnPopoverContentDirective, BrnPopoverTriggerDirective } from '@spartan-ng/ui-popover-brain';
-import { HlmScrollAreaComponent } from '@spartan-ng/ui-scrollarea-helm';
-import { HlmIconComponent, provideIcons } from '@spartan-ng/ui-icon-helm';
-
-import { RouterLink } from '@angular/router';
-import { SecurityStore } from '@app/core/security/security-store.service';
-import { cn } from '@app/utils';
-import { lucideCheck, lucideClipboardCopy } from '@ng-icons/lucide';
+import { ReviewsPopoverComponent } from './reviews-popover/reviews-popover.component';
 
 @Component({
   selector: 'app-leaderboard',
@@ -33,17 +27,10 @@ import { lucideCheck, lucideClipboardCopy } from '@ng-icons/lucide';
     TableHeaderDirective,
     TableHeadDirective,
     TableRowDirective,
+    ReviewsPopoverComponent,
     NgIconComponent,
-    RouterLink,
-    HlmPopoverModule,
-    BrnPopoverComponent,
-    BrnPopoverContentDirective,
-    BrnPopoverTriggerDirective,
-    HlmScrollAreaComponent,
-    HlmButtonModule,
-    HlmIconComponent
+    RouterLink
   ],
-  providers: [provideIcons({ lucideClipboardCopy, lucideCheck })],
   templateUrl: './leaderboard.component.html'
 })
 export class LeaderboardComponent {
@@ -61,38 +48,8 @@ export class LeaderboardComponent {
 
   leaderboard = input<LeaderboardEntry[]>();
   isLoading = input<boolean>();
-  showCopySuccess = signal(false);
 
   trClass = (entry: LeaderboardEntry) => {
     return cn('cursor-pointer', this.signedIn() && this.user()?.username.toLowerCase() === entry.user.login.toLowerCase() ? 'bg-accent' : '');
-  };
-
-  calcScrollHeight = (arr: PullRequestInfo[]) => {
-    return `min(200px, calc(${15 + 42 * arr.length}px))`;
-  };
-  displayPullRequestTitle = (title: string) => title.replace(/`([^`]+)`/g, '<code class="textCode">$1</code>');
-
-  copyPullRequests = (entry: LeaderboardEntry) => {
-    const htmlList = `<ul>
-      ${entry.reviewedPullRequests.map((pr) => `<li><a href="${pr.htmlUrl}">#${pr.number}</a></li>`).join('\n')}
-    </ul>`;
-
-    const plainText = entry.reviewedPullRequests.map((pr) => `#${pr.number}`).join('\n');
-
-    const clipboardItem = new ClipboardItem({
-      'text/html': new Blob([htmlList], { type: 'text/html' }),
-      'text/plain': new Blob([plainText], { type: 'text/plain' })
-    });
-
-    navigator.clipboard.write([clipboardItem]).catch(() => {
-      // Fallback to plain text if html copying fails
-      navigator.clipboard.writeText(plainText);
-    });
-
-    this.showCopySuccess.set(true);
-
-    setTimeout(() => {
-      this.showCopySuccess.set(false);
-    }, 2000);
   };
 }
