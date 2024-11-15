bdcd5c40e0022c68e7d3d1a8f66005b3ff9ab95a
Armin Stanitzok <21990230+GODrums@users.noreply.github.com>
2024-11-15 18:15:17 +0100

    Popover for reviewed PRs & Copy-Button (#162)
    Co-authored-by: Felix T.J. Dietrich <felix_dietrich@gmx.de>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- webapp/src/app/home/leaderboard/leaderboard.component.ts
+++ webapp/src/app/home/leaderboard/leaderboard.component.ts
@@ -1,20 +1,19 @@
+import { RouterLink } from '@angular/router';
 import { Component, inject, input } from '@angular/core';
 import { NgIconComponent } from '@ng-icons/core';
 import { octFileDiff, octCheck, octComment, octCommentDiscussion, octGitPullRequest, octChevronLeft, octNoEntry } from '@ng-icons/octicons';
+import { cn } from '@app/utils';
+import { HlmAvatarModule } from '@spartan-ng/ui-avatar-helm';
+import { HlmSkeletonModule } from '@spartan-ng/ui-skeleton-helm';
+import { SecurityStore } from '@app/core/security/security-store.service';
 import { LeaderboardEntry } from 'app/core/modules/openapi';
 import { TableBodyDirective } from 'app/ui/table/table-body.directive';
-import { TableCaptionDirective } from 'app/ui/table/table-caption.directive';
 import { TableCellDirective } from 'app/ui/table/table-cell.directive';
-import { TableFooterDirective } from 'app/ui/table/table-footer.directive';
 import { TableHeadDirective } from 'app/ui/table/table-head.directive';
 import { TableHeaderDirective } from 'app/ui/table/table-header.directive';
 import { TableRowDirective } from 'app/ui/table/table-row.directive';
 import { TableComponent } from 'app/ui/table/table.component';
-import { HlmAvatarModule } from '@spartan-ng/ui-avatar-helm';
-import { HlmSkeletonModule } from '@spartan-ng/ui-skeleton-helm';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { SecurityStore } from '@app/core/security/security-store.service';
-import { cn } from '@app/utils';
+import { ReviewsPopoverComponent } from './reviews-popover/reviews-popover.component';
 
 @Component({
   selector: 'app-leaderboard',
@@ -24,15 +23,13 @@ import { cn } from '@app/utils';
     HlmSkeletonModule,
     TableComponent,
     TableBodyDirective,
-    TableCaptionDirective,
     TableCellDirective,
-    TableFooterDirective,
     TableHeaderDirective,
     TableHeadDirective,
     TableRowDirective,
+    ReviewsPopoverComponent,
     NgIconComponent,
-    RouterLink,
-    RouterOutlet
+    RouterLink
   ],
   templateUrl: './leaderboard.component.html'
 })
@@ -49,10 +46,10 @@ export class LeaderboardComponent {
   signedIn = this.securityStore.signedIn;
   user = this.securityStore.loadedUser;
 
+  leaderboard = input<LeaderboardEntry[]>();
+  isLoading = input<boolean>();
+
   trClass = (entry: LeaderboardEntry) => {
     return cn('cursor-pointer', this.signedIn() && this.user()?.username.toLowerCase() === entry.user.login.toLowerCase() ? 'bg-accent' : '');
   };
-
-  leaderboard = input<LeaderboardEntry[]>();
-  isLoading = input<boolean>();
 }
