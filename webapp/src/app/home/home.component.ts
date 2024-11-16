c92eded706d75fc846f626d1fa7bf6e231e4fab2
GODrums <armin.stanitzok@outlook.de>
2024-11-17 00:05:48 +0100

    Leaderboard legend

--- webapp/src/app/home/home.component.ts
+++ webapp/src/app/home/home.component.ts
@@ -12,13 +12,14 @@ import { LeaderboardFilterComponent } from './leaderboard/filter/filter.componen
 import { SecurityStore } from '@app/core/security/security-store.service';
 import { HlmAlertModule } from '@spartan-ng/ui-alert-helm';
 import { MetaService } from '@app/core/modules/openapi';
+import { LeaderboardLegendComponent } from "./leaderboard/legend/legends.component";
 
 dayjs.extend(isoWeek);
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LeaderboardComponent, LeaderboardFilterComponent, HlmAlertModule, LucideAngularModule],
+  imports: [LeaderboardComponent, LeaderboardFilterComponent, HlmAlertModule, LucideAngularModule, LeaderboardLegendComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
