4c8e56bfe348004d6a84f9e7f2e37026c014315b
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-11-19 19:54:53 +0100

    add imprint

---
+++ webapp/src/app/legal/imprint/imprint.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { environment } from 'environments/environment';
+
+@Component({
+  selector: 'app-imprint',
+  standalone: true,
+  template: `
+    <div class="prose dark:prose-invert">
+      <h1>Imprint</h1>
+      <div [innerHTML]="imprintHtml"></div>
+    </div>
+  `
+})
+export class ImprintComponent {
+  imprintHtml = environment.legal.imprintHtml;
+}
