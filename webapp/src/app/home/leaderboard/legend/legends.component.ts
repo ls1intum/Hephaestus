14a85f96f89c57fbb10cd7781c3e2a6af849abfd
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-11-20 09:40:36 +0100

    use accordion

--- webapp/src/app/home/leaderboard/legend/legends.component.ts
+++ webapp/src/app/home/leaderboard/legend/legends.component.ts
@@ -1,18 +1,31 @@
-import { Component, computed, input, signal } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { NgIconComponent } from '@ng-icons/core';
 import { octFileDiff, octCheck, octComment, octCommentDiscussion, octGitPullRequest } from '@ng-icons/octicons';
 import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
 import { HlmCardModule } from '@spartan-ng/ui-card-helm';
-import { provideIcons } from '@spartan-ng/ui-icon-helm';
-import { lucideChevronDown, lucideChevronUp } from '@ng-icons/lucide';
-import { cn } from '@app/utils';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import {
+  HlmAccordionContentComponent,
+  HlmAccordionDirective,
+  HlmAccordionIconDirective,
+  HlmAccordionItemDirective,
+  HlmAccordionTriggerDirective
+} from '@spartan-ng/ui-accordion-helm';
 
 @Component({
   selector: 'app-leaderboard-legend',
   standalone: true,
-  imports: [HlmCardModule, NgIconComponent, HlmIconComponent, HlmButtonDirective],
-  providers: [provideIcons({ lucideChevronDown, lucideChevronUp })],
+  imports: [
+    HlmAccordionDirective,
+    HlmAccordionItemDirective,
+    HlmAccordionTriggerDirective,
+    HlmAccordionContentComponent,
+    HlmAccordionIconDirective,
+    HlmCardModule,
+    NgIconComponent,
+    HlmIconComponent,
+    HlmButtonDirective
+  ],
   templateUrl: './legend.component.html'
 })
 export class LeaderboardLegendComponent {
@@ -23,11 +36,4 @@ export class LeaderboardLegendComponent {
   protected octGitPullRequest = octGitPullRequest;
 
   isLoading = input<boolean>();
-  open = signal(false);
-
-  contentClass = computed(() => cn('max-w-[500px]', { hidden: !this.open() }));
-
-  toggleOpen() {
-    this.open.set(!this.open());
-  }
 }
