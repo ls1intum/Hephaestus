c7d9c016afbd9d91cf956bf6c9490f6606f4f6fb
milenasrb <mi.serbinova@gmail.com>
2024-11-21 10:41:36 +0100

    Add autoscrolling

--- webapp/src/app/chat/messages/messages.component.ts
+++ webapp/src/app/chat/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, OnInit, AfterViewChecked, ElementRef, ViewChild} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LucideAngularModule, Hammer } from 'lucide-angular';
 import { HlmAvatarModule } from '@spartan-ng/ui-avatar-helm';
@@ -11,11 +11,29 @@ import { Message } from '@app/core/modules/openapi';
   standalone: true,
   imports: [CommonModule, LucideAngularModule, HlmAvatarModule,],
 })
-export class MessagesComponent {
+
+export class MessagesComponent implements OnInit, AfterViewChecked {
   protected Hammer = Hammer;
 
   securityStore = inject(SecurityStore);
   user = this.securityStore.loadedUser;
 
+  @ViewChild('chatMessagesContainer') private chatMessagesContainer!: ElementRef;
   @Input() messages: { sender: Message.SenderEnum; content: string; timestamp: string }[] = [];
+
+    ngOnInit() { 
+        this.scrollToBottom();
+    }
+
+    ngAfterViewChecked() {        
+        this.scrollToBottom();        
+    } 
+
+    private scrollToBottom(): void {
+      try {
+        this.chatMessagesContainer.nativeElement.scrollTop = this.chatMessagesContainer.nativeElement.scrollHeight;
+      } catch (err) {
+        console.error('Error scrolling to bottom', err);
+      }
+    }
 }
