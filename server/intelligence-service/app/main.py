c504570a8555496de931a6d2e45e4150c313347d
milenasrb <mi.serbinova@gmail.com>
2024-11-24 13:31:38 +0100

    Update FastAPI connection

--- server/intelligence-service/app/main.py
+++ server/intelligence-service/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from .model import chat
+from .model import send_message
 
 app = FastAPI(
     title="Hephaestus Intelligence Service API",
@@ -25,10 +25,12 @@ class ChatResponse(BaseModel):
     summary="Start and continue a chat session with an LLM.",
 )
 async def chat(request: ChatRequest):
-        # TODO: Add state management when implementing more complex chat logic
-        state = 0
-        user_input = request.message_content
-        result = chat(request.session_id, user_input, state)
-        state = result["state"]
+        # TODO: Add step management when implementing more complex chat logic
+        state = {"messages": [], "step": 0}
+        
+        result = send_message(thread_id=request.session_id, 
+                              input_message=request.message_content, 
+                              state=state)
+        
         response_message = result["response"]["messages"][-1].content
         return ChatResponse(session_id=request.session_id, message_content=response_message)
