82d283c22e216a6884970b802d16b8477f7d8a20
milenasrb <mi.serbinova@gmail.com>
2024-11-24 13:56:11 +0100

    Formating

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/chat/session/SessionRepository.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/chat/session/SessionRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Long> {
-    
+
     Optional<List<Session>> findByUserLogin(@Param("login") String login);
 }
