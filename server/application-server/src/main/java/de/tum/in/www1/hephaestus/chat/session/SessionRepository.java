25c63d42ac90cbed7ea66aa0ff26624e621875d5
milenasrb <mi.serbinova@gmail.com>
2024-11-25 17:41:24 +0100

    SImpllify code

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/chat/session/SessionRepository.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/chat/session/SessionRepository.java
@@ -1,14 +1,12 @@
 package de.tum.in.www1.hephaestus.chat.session;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.List;
 
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Long> {
 
-    Optional<List<Session>> findByUserLogin(@Param("login") String login);
+    List<Session> findByUserLogin(String login);
 }
