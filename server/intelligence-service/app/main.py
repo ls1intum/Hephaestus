eda63ded0a8f8dee2983411d964691bff9f87756
milenasrb <mi.serbinova@gmail.com>
2024-12-01 19:34:01 +0100

    Update bot logic

--- server/intelligence-service/app/main.py
+++ server/intelligence-service/app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import os
-from enum import Enum
 from typing import List
-from .config import settings
+from config import settings
 from typing_extensions import Annotated, TypedDict
 from langchain.chat_models.base import BaseChatModel
 from langchain_openai import ChatOpenAI, AzureChatOpenAI
@@ -43,7 +42,7 @@ if os.getenv("GITHUB_ACTIONS") == "true":
     model = MockChatModel()
 
 elif settings.is_openai_available:
-    model = ChatOpenAI(temperature=2)
+    model = ChatOpenAI()
 elif settings.is_azure_openai_available:
     model = AzureChatOpenAI()
 else:
@@ -54,17 +53,19 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 
-propmt = "You are an AI mentor helping a students working on the software engineering projects embracing structured self-reflection practices. You need to guide the student through the set questions regarding their work on the project during the last week (sprint). Your value is the fact, that you help students to reflect on their past progress. Throughout the conversation you need to perform all of the following tasks in the given order: Task 1: Greet the student and say you are happy to start the session. Task 2: Ask the student about the overall progress on the project. Task 3: Ask the student about the challenges faced during the sprint referring to what he said about progress. Task 4: Ask about the plan for the next sprint. You need to understand at which stage in the conversation you are and what is the next task. Be polite, friendly and do not let the student drive the conversation to any other topic except for the current project. Do not make a questionaire out of the conversation, but rather make it a natural conversation."
+prompt = "You are an AI mentor helping a students working on the software engineering projects embracing structured self-reflection practices. You need to guide the student through the set questions regarding their work on the project during the last week (sprint). Your value is the fact, that you help students to reflect on their past progress. Throughout the conversation you need to perform all of the following tasks in the given order: Task 1: Greet the student and say you are happy to start the session. Task 2: Ask the student about the overall progress on the project. Task 3: Ask the student about the challenges faced during the sprint referring to what he said about progress. Task 4: Ask about the plan for the next sprint. You need to understand at which stage in the conversation you are from the message history and what is the next task. Be polite, friendly and do not let the student drive the conversation to any other topic except for the current project. Do not make a questionaire out of the conversation, but rather make it a natural conversation."
 
 
 def ai_mentor(state: State):
+    print(SystemMessage(content=prompt) + state["messages"])
     return {
-        "messages": [model.invoke([SystemMessage(content=propmt)] + state["messages"])]
+        "messages": [model.invoke(state["messages"] + [SystemMessage(content=prompt)])]
     }
 
 
 graph_builder = StateGraph(State)
 graph_builder.add_node("ai_mentor", ai_mentor)
+
 graph_builder.add_edge(START, "ai_mentor")
 graph_builder.add_edge("ai_mentor", END)
 graph = graph_builder.compile()
@@ -75,15 +76,12 @@ graph = graph_builder.compile()
     response_model=ChatResponse,
     summary="Start and continue a chat session with an LLM.",
 )
-async def chat(request: ChatRequest):
+def chat(request: ChatRequest):
     messages = []
     for message in request.message_history:
-        if message.author == "USER":
+        if message.sender == "USER":
             messages.append(HumanMessage(content=message.content))
         else:
             messages.append(AIMessage(content=message.content))
-
-    for event in graph.stream({"messages": messages}, stream_mode="values"):
-        responce_message = event["messages"][-1].content
-
-    return ChatResponse(responce=responce_message)
+    response_message = graph.invoke({"messages": messages})["messages"][-1].content
+    return ChatResponse(responce=response_message)
