0277cd35e9030f80392f13462da42f1b00e88f3e
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-12-08 16:40:04 +0100

    add intelligence service to compose

--- server/intelligence-service/app/main.py
+++ server/intelligence-service/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from pydantic import BaseModel
 import os
 from typing import List
@@ -83,3 +83,30 @@ def chat(request: ChatRequest):
             messages.append(AIMessage(content=message.content))
     response_message = graph.invoke({"messages": messages})["messages"][-1].content
     return ChatResponse(responce=response_message)
+
+
+class HealthCheck(BaseModel):
+    """Response model to validate and return when performing a health check."""
+
+    status: str = "OK"
+
+
+@app.get(
+    "/health",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
+    to ensure a robust container orchestration and management is in place. Other
+    services which rely on proper functioning of the API service will not deploy if this
+    endpoint returns any other HTTP status code except 200 (OK).
+    Returns:
+        HealthCheck: Returns a JSON response with the health status
+    """
+    return HealthCheck(status="OK")
