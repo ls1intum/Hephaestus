e9c67c91e8e3ef70923ff0b59fd14da501174243
GODrums <armin.stanitzok@outlook.de>
2025-01-04 22:18:09 +0100

    Push own entry to top

--- webapp/src/app/home/leaderboard/leaderboard.component.ts
+++ webapp/src/app/home/leaderboard/leaderboard.component.ts
@@ -1,12 +1,12 @@
 import { RouterLink } from '@angular/router';
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { NgIconComponent } from '@ng-icons/core';
 import { octFileDiff, octCheck, octComment, octCommentDiscussion, octGitPullRequest, octChevronLeft, octNoEntry } from '@ng-icons/octicons';
 import { cn } from '@app/utils';
 import { HlmAvatarModule } from '@spartan-ng/ui-avatar-helm';
 import { HlmSkeletonModule } from '@spartan-ng/ui-skeleton-helm';
 import { SecurityStore } from '@app/core/security/security-store.service';
-import { LeaderboardEntry } from 'app/core/modules/openapi';
+import { LeaderboardEntry, PullRequestInfo } from 'app/core/modules/openapi';
 import { TableBodyDirective } from 'app/ui/table/table-body.directive';
 import { TableCellDirective } from 'app/ui/table/table-cell.directive';
 import { TableHeadDirective } from 'app/ui/table/table-head.directive';
@@ -52,6 +52,37 @@ export class LeaderboardComponent {
 
   leaderboard = input<LeaderboardEntry[]>();
   isLoading = input<boolean>();
+
+  protected adjustedLeaderboard = computed(() => {
+    const entries = this.leaderboard();
+    if (!this.signedIn() || !entries || entries.length === 0) return entries;
+
+    const currentUser = this.user();
+    if (!currentUser) return entries;
+
+    const userInLeaderboard = entries.find((entry) => entry.user.login.toLowerCase() === currentUser.username);
+
+    return [userInLeaderboard ?? this.defaultSelfEntry(), ...entries];
+  });
+
+  defaultSelfEntry = computed(() => {
+    return {
+      user: {
+        login: this.user()?.username,
+        avatarUrl: `https://github.com/${this.user()?.username}.png`,
+        name: this.user()?.name
+      },
+      rank: this.leaderboard()?.length ?? 0 + 1,
+      score: 0,
+      numberOfApprovals: 0,
+      numberOfChangeRequests: 0,
+      numberOfComments: 0,
+      numberOfCodeComments: 0,
+      numberOfReviewedPRs: 0,
+      numberOfUnknowns: 0,
+      reviewedPullRequests: [] as PullRequestInfo[]
+    } as LeaderboardEntry;
+  });
 
   trClass = (entry: LeaderboardEntry) => {
     return cn(
