4e36b824e6fe384fd631b9ee860b5923b9dbb0d2
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-01-24 22:26:13 +0100

    change database stuff

--- server/intelligence-service/app/mentor/run.py
+++ server/intelligence-service/app/mentor/run.py
@@ -1,9 +1,11 @@
-from .state import State
-from langgraph.graph import START, StateGraph, END
 from psycopg_pool import ConnectionPool
+from langgraph.graph import START, StateGraph, END
 from langgraph.checkpoint.postgres import PostgresSaver
 from langgraph.store.postgres import PostgresStore
 from langchain_core.messages import HumanMessage
+
+from .state import State
+from ..settings import settings
 
 from .nodes import (
     greet,
@@ -18,13 +20,7 @@ from .nodes import (
 )
 from .conditions import start_router, main_router
 
-
-POSTGRES_CONFIG = {
-    "dbname": "hephaestus",
-    "user": "root",
-    "password": "root",
-    "host": "localhost",
-    "port": "5432",
+connection_kwargs = {
     "autocommit": True,
     "prepare_threshold": 0,
 }
@@ -54,12 +50,16 @@ graph_builder.add_edge("mentor_node", END)
 
 
 def start_session(last_thread: str, config):
-    with ConnectionPool(kwargs=POSTGRES_CONFIG) as pool:
+    with ConnectionPool(
+        conninfo=settings.DATABASE_CONNECTION_STRING,
+        max_size=20,
+        kwargs=connection_kwargs,
+    ) as pool:
         checkpointer = PostgresSaver(pool)
         checkpointer.setup()
 
         with PostgresStore.from_conn_string(
-            "postgresql://root:root@localhost:5432/hephaestus"
+            settings.DATABASE_CONNECTION_STRING
         ) as store:
             store.setup()
             graph = graph_builder.compile(checkpointer=checkpointer, store=store)
@@ -84,12 +84,16 @@ def start_session(last_thread: str, config):
 
 
 def run(message: str, config):
-    with ConnectionPool(kwargs=POSTGRES_CONFIG) as pool:
+    with ConnectionPool(
+        conninfo=settings.DATABASE_CONNECTION_STRING,
+        max_size=20,
+        kwargs=connection_kwargs,
+    ) as pool:
         checkpointer = PostgresSaver(pool)
         checkpointer.setup()
 
         with PostgresStore.from_conn_string(
-            "postgresql://root:root@localhost:5432/hephaestus"
+            settings.DATABASE_CONNECTION_STRING
         ) as store:
             store.setup()
             graph = graph_builder.compile(checkpointer=checkpointer, store=store)
--- server/intelligence-service/app/settings.py
+++ server/intelligence-service/app/settings.py
@@ -7,6 +7,10 @@ load_dotenv()
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
+    DATABASE_URL: str = "postgresql://localhost:5432/hephaestus"
+    DATABASE_USERNAME: str = "root"
+    DATABASE_PASSWORD: str = "root"
+
     OPENAI_API_KEY: str = ""
 
     AZURE_OPENAI_API_KEY: str = ""
@@ -28,6 +32,15 @@ class Settings(BaseSettings):
             and bool(self.AZURE_OPENAI_ENDPOINT)
             and bool(self.AZURE_OPENAI_API_VERSION)
         )
+
+    @property
+    def DATABASE_CONNECTION_STRING(self):
+        result = (
+            f"postgresql://{self.DATABASE_USERNAME}:{self.DATABASE_PASSWORD}@{self.DATABASE_URL.replace('postgresql://', '')}"
+            + "?sslmode=disable"
+        )
+        print(result)
+        return result
 
 
 settings = Settings()
