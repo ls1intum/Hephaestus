50eea97e6718b880be8a3498a44fd85ea35b3c07
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-02-23 23:17:40 +0100

    remove icons

--- webapp/src/app/home/leaderboard/leaderboard-overview/leaderboard-overview.component.ts
+++ webapp/src/app/home/leaderboard/leaderboard-overview/leaderboard-overview.component.ts
@@ -1,19 +1,14 @@
 import { Component, input } from '@angular/core';
-import { provideIcons } from '@ng-icons/core';
-import { lucideAward } from '@ng-icons/lucide';
-import { octFileDiff, octCheck, octComment, octCommentDiscussion, octGitPullRequest, octChevronLeft } from '@ng-icons/octicons';
 import { LeaderboardEntry } from '@app/core/modules/openapi';
 import { HlmCardModule } from '@spartan-ng/ui-card-helm';
 import { HlmButtonModule } from '@spartan-ng/ui-button-helm';
 import { LeagueEloCardComponent } from '@app/ui/league/elo-card/elo-card.component';
-import { HlmIconModule } from '@spartan-ng/ui-icon-helm';
 import { HlmAvatarModule } from '@spartan-ng/ui-avatar-helm';
 
 @Component({
   selector: 'app-leaderboard-overview',
-  imports: [HlmCardModule, HlmButtonModule, HlmAvatarModule, LeagueEloCardComponent, HlmIconModule],
-  templateUrl: './leaderboard-overview.component.html',
-  providers: [provideIcons({ lucideAward, octFileDiff, octCheck, octComment, octCommentDiscussion, octGitPullRequest, octChevronLeft })]
+  imports: [HlmCardModule, HlmButtonModule, HlmAvatarModule, LeagueEloCardComponent],
+  templateUrl: './leaderboard-overview.component.html'
 })
 export class LeaderboardOverviewComponent {
   leaderboardEntry = input.required<LeaderboardEntry>();
