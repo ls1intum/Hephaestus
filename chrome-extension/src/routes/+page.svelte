9e425c336534d2f4dd0beb072592155b087dcc4c
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-03-07 23:41:41 +0100

    add chrome extension

---
+++ chrome-extension/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  let message = 'Hello, world!';
+
+  async function handleClick() {
+    message = 'Button clicked!';
+
+    let [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id! },
+      func: () => {
+        document.body.style.backgroundColor = 'red';
+      }
+    });
+
+  }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<button on:click={handleClick}>
+  Click me
+</button>
+<p>{message}</p>
