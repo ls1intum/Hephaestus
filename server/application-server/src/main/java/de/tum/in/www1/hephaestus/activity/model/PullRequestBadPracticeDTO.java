78c21e9a22dfa78735c053036ee009371e94cf61
Florian Ehrenstorfer <flo.ehrenstorfer@tum.de>
2025-03-17 14:37:45 +0100

    update user resolve and feedback endpoint #287

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/activity/model/PullRequestBadPracticeDTO.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/activity/model/PullRequestBadPracticeDTO.java
@@ -1,8 +1,11 @@
 package de.tum.in.www1.hephaestus.activity.model;
 
-public record PullRequestBadPracticeDTO(String title, String description, boolean resolved, boolean userResolved) {
+import lombok.NonNull;
+
+public record PullRequestBadPracticeDTO(@NonNull Long id, String title, String description, boolean resolved, boolean userResolved) {
     public static PullRequestBadPracticeDTO fromPullRequestBadPractice(PullRequestBadPractice badPractice) {
         return new PullRequestBadPracticeDTO(
+            badPractice.getId(),
             badPractice.getTitle(),
             badPractice.getDescription(),
             badPractice.isResolved(),
