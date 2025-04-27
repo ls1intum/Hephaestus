29c17858b0a01d67c1bf24c6ad97b1516bbf4976
Florian Ehrenstorfer <flo.ehrenstorfer@tum.de>
2025-04-27 12:50:52 +0200

    update

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/activity/model/PullRequestBadPracticeDTO.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/activity/model/PullRequestBadPracticeDTO.java
@@ -7,16 +7,15 @@ public record PullRequestBadPracticeDTO(
     @NonNull Long id,
     @NonNull String title,
     @NonNull String description,
-    @NonNull PullRequestBadPracticeState state,
-    @Nullable PullRequestBadPracticeState userState
-) {
+    @NonNull PullRequestBadPracticeState state) {
     public static PullRequestBadPracticeDTO fromPullRequestBadPractice(PullRequestBadPractice badPractice) {
         return new PullRequestBadPracticeDTO(
             badPractice.getId(),
             badPractice.getTitle(),
             badPractice.getDescription(),
-            badPractice.getState(),
-            badPractice.getUserState()
+            badPractice.getUserState() != null
+                ? badPractice.getUserState()
+                : badPractice.getState()
         );
     }
 }
