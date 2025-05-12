08174f81ce567b0c9def73c589fa3f45a342101f
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-05-12 14:35:10 +0200

    Release: Bump version to 0.7.2 (patch update)

--- server/intelligence-service/app/main.py
+++ server/intelligence-service/app/main.py
@@ -7,7 +7,7 @@ from app.routers.detector import router as detector_router
 app = FastAPI(
     title="Hephaestus Intelligence Service API",
     description="API documentation for the Hephaestus Intelligence Service.",
-    version="0.7.1",
+    version="0.7.2",
     contact={"name": "Felix T.J. Dietrich", "email": "felixtj.dietrich@tum.de"},
 )
 
