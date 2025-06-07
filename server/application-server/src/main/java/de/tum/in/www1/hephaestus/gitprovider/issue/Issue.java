60b7cbb7228403d455f3df18419976beb4a8d867
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-06-07 16:42:57 +0200

    fix: rename workflow from "Database Migration Validation" to "DB QA" and remove temporary title field in Issue class

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/gitprovider/issue/Issue.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/gitprovider/issue/Issue.java
@@ -53,9 +53,6 @@ public class Issue extends BaseGitServiceEntity {
     @NonNull
     private String title;
 
-    @NonNull
-    private String tmpTitle;
-
     @Lob
     @ToString.Exclude
     private String body;
