62e7e064b1a60183ada4d1f9d8fb8510b4e37336
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-06-08 20:56:59 +0200

    refactor: Update integration test configuration to use PostgreSQLTestContainer for improved database management

--- server/application-server/src/test/java/de/tum/in/www1/hephaestus/HephaestusApplicationTests.java
+++ server/application-server/src/test/java/de/tum/in/www1/hephaestus/HephaestusApplicationTests.java
@@ -1,12 +1,13 @@
 package de.tum.in.www1.hephaestus;
 
+import de.tum.in.www1.hephaestus.testconfig.PostgreSQLTestContainer;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 /**
@@ -16,16 +17,12 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @SpringBootTest
 @ActiveProfiles("test")
 @Testcontainers
+@Tag("integration")
 class HephaestusApplicationTests {
-
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
-        .withDatabaseName("hephaestus_test")
-        .withUsername("test")
-        .withPassword("test");
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
+        PostgreSQLContainer<?> postgres = PostgreSQLTestContainer.getInstance();
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
