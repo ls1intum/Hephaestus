600a2c0ca1e3cdeb5e132c04f73e41c63af54668
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-06-19 09:50:34 +0200

    fix: Update ChatRequestDTO to use UIMessage instead of Message for consistency

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/mentor/ChatRequestDTO.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/mentor/ChatRequestDTO.java
@@ -1,12 +1,12 @@
 package de.tum.in.www1.hephaestus.mentor;
 
-import de.tum.in.www1.hephaestus.intelligenceservice.model.Message;
+import de.tum.in.www1.hephaestus.intelligenceservice.model.UIMessage;
 import org.springframework.lang.NonNull;
 
 import java.util.List;
 
 public record ChatRequestDTO(
     @NonNull String id,
-    @NonNull List<Message> messages
+    @NonNull List<UIMessage> messages
 ) {
 }
