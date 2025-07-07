42bfc6f65acb9ca193fec0b89ca9d8ff52d53f85
Dmytro Dmukh <106586393+ddmukh@users.noreply.github.com>
2025-07-07 17:21:50 +0200

    feat(application-server): add GitHub team synchronization (#342)
    Co-authored-by: Dmytro Dmukh <dmytro.dmukh@tum.de>
    Co-authored-by: Felix T.J. Dietrich <felixtj.dietrich@tum.de>

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/gitprovider/common/github/GitHubMessageHandlerRegistry.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/gitprovider/common/github/GitHubMessageHandlerRegistry.java
@@ -10,19 +10,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class GitHubMessageHandlerRegistry {
 
-    private final Map<GHEvent, GitHubMessageHandler<?>> handlerMap = new HashMap<>();
+    private final Map<GHEvent, GitHubMessageHandler<?>> repositoryHandlerMap = new HashMap<>();
+    private final Map<GHEvent, GitHubMessageHandler<?>> organizationHandlerMap = new HashMap<>();
 
     public GitHubMessageHandlerRegistry(GitHubMessageHandler<?>[] handlers) {
         for (GitHubMessageHandler<?> handler : handlers) {
-            handlerMap.put(handler.getHandlerEvent(), handler);
+            if (handler.isOrganizationEvent()) {
+                organizationHandlerMap.put(handler.getHandlerEvent(), handler);
+            } else {
+                repositoryHandlerMap.put(handler.getHandlerEvent(), handler);
+            }
         }
     }
 
     public GitHubMessageHandler<?> getHandler(GHEvent eventType) {
-        return handlerMap.get(eventType);
+        return repositoryHandlerMap.getOrDefault(eventType, organizationHandlerMap.get(eventType));
     }
 
-    public List<GHEvent> getSupportedEvents() {
-        return new ArrayList<>(handlerMap.keySet());
+    public List<GHEvent> getSupportedRepositoryEvents() {
+        return new ArrayList<>(repositoryHandlerMap.keySet());
+    }
+
+    public List<GHEvent> getSupportedOrganizationEvents() {
+        return new ArrayList<>(organizationHandlerMap.keySet());
     }
 }
