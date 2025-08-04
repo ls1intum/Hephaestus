b8b026acf75c4fb4e9f6c1b0c56e24764858a6b4
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-08-04 17:52:55 +0200

    add text artifact types

---
+++ webapp-react/src/components/mentor/Artifact.tsx
@@ -0,0 +1,22 @@
+import { textArtifact } from './artifacts/text';
+
+export const artifactDefinitions = [
+  textArtifact,
+];
+
+export type ArtifactKind = (typeof artifactDefinitions)[number]['kind'];
+
+export interface UIArtifact {
+  title: string;
+  documentId: string;
+  kind: ArtifactKind;
+  content: string;
+  isVisible: boolean;
+  status: 'streaming' | 'idle';
+  boundingBox: {
+    top: number;
+    left: number;
+    width: number;
+    height: number;
+  };
+}
