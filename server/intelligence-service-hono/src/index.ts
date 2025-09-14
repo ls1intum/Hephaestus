d7afad2181e6a8cd3a8797c3820ebc685bfbc8f2
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-09-14 10:06:15 +0200

    init hono

---
+++ server/intelligence-service-hono/src/index.ts
@@ -0,0 +1,15 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+serve({
+  fetch: app.fetch,
+  port: 8000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
