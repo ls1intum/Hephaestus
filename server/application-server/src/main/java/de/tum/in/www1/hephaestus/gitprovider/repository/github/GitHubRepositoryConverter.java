1b274604e67d42601d0066754d33228e919ce451
Dmytro Dmukh <dmytro.dmukh@tum.de>
2025-09-23 17:01:00 +0200

    Update repo converter

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/gitprovider/repository/github/GitHubRepositoryConverter.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/gitprovider/repository/github/GitHubRepositoryConverter.java
@@ -2,17 +2,25 @@ package de.tum.in.www1.hephaestus.gitprovider.repository.github;
 
 import de.tum.in.www1.hephaestus.gitprovider.common.BaseGitServiceEntityConverter;
 import de.tum.in.www1.hephaestus.gitprovider.repository.Repository;
+import de.tum.in.www1.hephaestus.organization.Organization;
+import de.tum.in.www1.hephaestus.organization.OrganizationService;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GHRepository.Visibility;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class GitHubRepositoryConverter extends BaseGitServiceEntityConverter<GHRepository, Repository> {
 
     private static final Logger logger = LoggerFactory.getLogger(GitHubRepositoryConverter.class);
+
+    @Autowired
+    OrganizationService organizationService;
 
     @Override
     public Repository convert(@NonNull GHRepository source) {
@@ -38,6 +46,20 @@ public class GitHubRepositoryConverter extends BaseGitServiceEntityConverter<GHR
         repository.setHasIssues(source.hasIssues());
         repository.setHasProjects(source.hasProjects());
         repository.setHasWiki(source.hasWiki());
+
+        try {
+            var owner = source.getOwner();
+            if (owner != null && "Organization".equalsIgnoreCase(owner.getType())) {
+                long orgId = owner.getId();
+                String orgLogin = owner.getLogin();
+                Organization org = organizationService.upsertIdentity(orgId, orgLogin);
+
+                repository.setOrganization(org);
+            }
+        } catch (IOException e) {
+            logger.error("Couldn't fetch organization owner for repository: {}", source.getName());
+        }
+
         return repository;
     }
 
