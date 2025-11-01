c30969516428c6e226c456660b60d55c0d9a65bd
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-11-01 21:02:53 +0100

    fix ci

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/workspace/WorkspaceStartupListener.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/workspace/WorkspaceStartupListener.java
@@ -3,6 +3,7 @@ package de.tum.in.www1.hephaestus.workspace;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
  * Spring lifecycle hook that kicks off workspace provisioning and monitoring once the application is ready.
  */
 @Component
+@Profile("!specs")
 public class WorkspaceStartupListener {
 
     private static final Logger logger = LoggerFactory.getLogger(WorkspaceStartupListener.class);
