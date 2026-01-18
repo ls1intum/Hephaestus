1d828f97aa1802f36a0a53effbcb462ed1d41c30
Max Achterberg <max.achterberg@tum.de>
2026-01-18 23:33:54 +0100

    chore(webapp): updated package lock and removed unnecessary tailwind.config.js

--- webapp/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
