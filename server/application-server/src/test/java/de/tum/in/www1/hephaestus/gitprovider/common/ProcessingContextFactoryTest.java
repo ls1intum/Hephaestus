3b2f975cc1527b95f97ee2248576f9366c4bdcdd
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2026-01-30 08:50:17 +0100

    style: format test file

--- server/application-server/src/test/java/de/tum/in/www1/hephaestus/gitprovider/common/ProcessingContextFactoryTest.java
+++ server/application-server/src/test/java/de/tum/in/www1/hephaestus/gitprovider/common/ProcessingContextFactoryTest.java
@@ -107,7 +107,9 @@ class ProcessingContextFactoryTest {
             when(repository.getNameWithOwner()).thenReturn(repoFullName);
 
             when(repositoryScopeFilter.isRepositoryAllowed(repoFullName)).thenReturn(true);
-            when(repositoryRepository.findByNameWithOwnerWithOrganization(repoFullName)).thenReturn(Optional.of(repository));
+            when(repositoryRepository.findByNameWithOwnerWithOrganization(repoFullName)).thenReturn(
+                Optional.of(repository)
+            );
             when(scopeIdResolver.findScopeIdByRepositoryName(repoFullName)).thenReturn(Optional.of(42L));
 
             // Act
@@ -164,7 +166,9 @@ class ProcessingContextFactoryTest {
             when(repository.getNameWithOwner()).thenReturn(repoFullName);
 
             when(repositoryScopeFilter.isRepositoryAllowed(repoFullName)).thenReturn(true);
-            when(repositoryRepository.findByNameWithOwnerWithOrganization(repoFullName)).thenReturn(Optional.of(repository));
+            when(repositoryRepository.findByNameWithOwnerWithOrganization(repoFullName)).thenReturn(
+                Optional.of(repository)
+            );
             when(scopeIdResolver.findScopeIdByOrgLogin(orgLogin)).thenReturn(Optional.of(expectedScopeId));
 
             // Act
@@ -190,7 +194,9 @@ class ProcessingContextFactoryTest {
             when(repository.getNameWithOwner()).thenReturn(repoFullName);
 
             when(repositoryScopeFilter.isRepositoryAllowed(repoFullName)).thenReturn(true);
-            when(repositoryRepository.findByNameWithOwnerWithOrganization(repoFullName)).thenReturn(Optional.of(repository));
+            when(repositoryRepository.findByNameWithOwnerWithOrganization(repoFullName)).thenReturn(
+                Optional.of(repository)
+            );
             when(scopeIdResolver.findScopeIdByRepositoryName(repoFullName)).thenReturn(Optional.of(expectedScopeId));
 
             // Act
@@ -221,7 +227,9 @@ class ProcessingContextFactoryTest {
             when(repository.getNameWithOwner()).thenReturn(repoFullName);
 
             when(repositoryScopeFilter.isRepositoryAllowed(repoFullName)).thenReturn(true);
-            when(repositoryRepository.findByNameWithOwnerWithOrganization(repoFullName)).thenReturn(Optional.of(repository));
+            when(repositoryRepository.findByNameWithOwnerWithOrganization(repoFullName)).thenReturn(
+                Optional.of(repository)
+            );
             // Org lookup fails
             when(scopeIdResolver.findScopeIdByOrgLogin(orgLogin)).thenReturn(Optional.empty());
             // Fallback to repo lookup succeeds
@@ -251,7 +259,9 @@ class ProcessingContextFactoryTest {
             when(repository.getNameWithOwner()).thenReturn(repoFullName);
 
             when(repositoryScopeFilter.isRepositoryAllowed(repoFullName)).thenReturn(true);
-            when(repositoryRepository.findByNameWithOwnerWithOrganization(repoFullName)).thenReturn(Optional.of(repository));
+            when(repositoryRepository.findByNameWithOwnerWithOrganization(repoFullName)).thenReturn(
+                Optional.of(repository)
+            );
             when(scopeIdResolver.findScopeIdByRepositoryName(repoFullName)).thenReturn(Optional.empty());
 
             // Act
