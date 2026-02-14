85a971a2470ab38e182abaf4533cab8cfdf67811
Max Achterberg <max.achterberg@tum.de>
2026-02-14 15:23:47 +0100

    feat(server): enhance LinearAchievementProgress with validation annotations

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/achievement/progress/LinearAchievementProgress.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/achievement/progress/LinearAchievementProgress.java
@@ -1,12 +1,18 @@
 package de.tum.in.www1.hephaestus.achievement.progress;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
-import org.springframework.lang.NonNull;
+import jakarta.validation.constraints.PositiveOrZero;
 
 @Schema(name = "LinearAchievementProgress", description = "Linear progress with current and target counts")
 public record LinearAchievementProgress(
-    @NonNull int current,
-    @NonNull int target
+    @PositiveOrZero
+    @JsonProperty(required = true)
+    int current,
+
+    @PositiveOrZero
+    @JsonProperty(required = true)
+    int target
 ) implements AchievementProgress {
     public LinearAchievementProgress(int target) {
         this(0, target);
