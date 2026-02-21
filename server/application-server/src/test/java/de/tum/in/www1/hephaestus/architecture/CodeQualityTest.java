e7186302aaca259bc62aced4d384d678df65705b
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2026-02-21 13:50:07 +0100

    fix(ci): allow CommitRepository.updateEnrichmentMetadata in parameter count rule
    Native SQL repository methods require individual @Param annotations per
    column and cannot use parameter objects. Add to the allow list alongside
    the existing upsertCommit exemption.

--- server/application-server/src/test/java/de/tum/in/www1/hephaestus/architecture/CodeQualityTest.java
+++ server/application-server/src/test/java/de/tum/in/www1/hephaestus/architecture/CodeQualityTest.java
@@ -159,7 +159,8 @@ class CodeQualityTest extends HephaestusArchitectureTest {
                 "ProjectFieldValueRepository.upsertCore",
                 "ProjectStatusUpdateRepository.upsertCore",
                 "UserRepository.upsertUser",
-                "CommitRepository.upsertCommit"
+                "CommitRepository.upsertCommit",
+                "CommitRepository.updateEnrichmentMetadata"
             );
 
             ArchCondition<JavaClass> haveMethodsWithLimitedParams = new ArchCondition<>(
