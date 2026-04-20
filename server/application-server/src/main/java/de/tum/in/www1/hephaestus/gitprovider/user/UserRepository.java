d89800fcdd023814b35f3c2ef34d9304a840d25d
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2026-04-20 08:04:36 +0200

    fix(server): restore user repository transactional import

--- server/application-server/src/main/java/de/tum/in/www1/hephaestus/gitprovider/user/UserRepository.java
+++ server/application-server/src/main/java/de/tum/in/www1/hephaestus/gitprovider/user/UserRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query(
